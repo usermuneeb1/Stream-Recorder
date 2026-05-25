882188f387046ee7bb15e5192e5563933d55ec82
Muneeb Ahmad <muneeb@stream-recorder.local>
2026-05-25 06:31:10 +0500

    🐛 Fix routing and base path for GitHub Pages

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -4,16 +4,16 @@ import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './contexts/ThemeContext.tsx'
 import { GithubProvider } from './contexts/GithubContext.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <GithubProvider>
           <App />
         </GithubProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
